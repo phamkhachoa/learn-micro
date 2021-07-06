4c0ba9143c9ad82f873071a5e0166efeedc1481c
Hoa Pham <hoa.pham@lifesup.com.vn>
2021-07-07 00:31:04 +0700

    new

---
+++ product_micro/products-service/src/main/java/com/appsdeveloperblog/estore/ProductsService/rest/ProductsController.java
@@ -0,0 +1,56 @@
+package com.appsdeveloperblog.estore.ProductsService.rest;
+
+import com.appsdeveloperblog.estore.ProductsService.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    public ProductsController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
+
+    @PostMapping
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        String returnValue;
+
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+
+        return returnValue;
+    }
+
+    @GetMapping
+    public String getProduct() {
+        return "HTTP GET Handled" + env.getProperty("local.server.port");
+    }
+
+    @PutMapping
+    public String updateProduct() {
+        return "HTTP PUT Handled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct() {
+        return "HTTP DELETE Handled";
+    }
+}
